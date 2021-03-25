1763241e5440375e30a262b83dfac3fb198d3ef3
Kisslatka <kisslatka1990@gmail.com>
2021-03-25 19:43:05 +0100

    03.25

--- test/Program.cs
+++ test/Program.cs
@@ -6,7 +6,22 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a;
+
+            do
+            {
+                Console.WriteLine("Adj meg egy számot!");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                if (a % 2 == 0) Console.WriteLine("A szám osztható 2-vel");
+                if (a % 3 == 0) Console.WriteLine("A szám osztható 3-mal");
+                if (a % 5 == 0) Console.WriteLine("A szám oszható 5-tel");
+
+                Console.ReadKey();
+
+
+            } while (a != 0);
+
         }
     }
 }
